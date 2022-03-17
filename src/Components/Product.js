012e6351236822adec267b5a7e1fc152a5e17e29
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-03-17 09:32:32 +0100

    aaa

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-export const Product = () => {
+export const Product = (props) => {
   return (
     <div class="productgallery_card">
         <div class="productgallery_card_container">
-            <img src="https://picsum.photos/300/300?1" alt="cookies" class="hero-image"/>
+            <img src={props.product.image} alt="cookies" class="hero-image"/>
             <div class="information">
 
-                <div class="name">Lorem, ipsum.</div>
+                <div class="name">{props.product.title}</div>
 
-                <div class="store">Lorem, ipsum dolor.</div>
+                <div class="store">{props.product.category}</div>
 
                 <a href="#" class="storebutton">Purchase Product</a>
 
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Product } from './Product'
+import { products } from './Data/ProductData'
 
 export const Products = () => {
+
   return (
+
     
     <section class="products" id="products">
     <h1 class="heading"> <span>our</span> products </h1>
 
     <div class="productgallery">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {products.map((p)=>  <Product product={p} />   )  }
+        {/* <Product product={produkten} /> */}
     </div>
 
 </section>
